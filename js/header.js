45ec3c10b26a4bbdc1dc3ae8379bb844982c3d4b
ngatti68 <norberto.o.gatti@gmail.com>
2024-05-18 21:50:43 -0300

    Primer commit del proyecto

---
+++ js/header.js
@@ -0,0 +1,14 @@
+let miHeader = `
+<div id="logo">
+    <a href="/">DULCE<span id="bombon">BOMBÓN</span></a>
+</div>
+<nav>
+    <a href="/">INICIO</a>
+    <a href="./about-us.html">QUIENES SOMOS</a>
+    <a href="./products.html">PRODUCTOS</a>
+    <a href="./contact.html">CONTACTO</a>
+    <a href="./shopping-cart"><i class="fa-solid fa-bag-shopping"></i></a>
+</nav>
+<hr>
+`
+document.querySelector("header").innerHTML = miHeader
